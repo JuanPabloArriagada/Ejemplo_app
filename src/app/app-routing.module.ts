f866b9f1ba728605849b0c73cf7472cb67a5e320
Juan Pablo Arr <ju.arriagadar@duocuc.cl>
2024-08-29 10:03:12 -0400

    pagina de alertas y botones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,14 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
+  {
+    path: 'botones',
+    loadChildren: () => import('./pages/botones/botones.module').then( m => m.BotonesPageModule)
+  },
+  {
+    path: 'alertas',
+    loadChildren: () => import('./pages/alertas/alertas.module').then( m => m.AlertasPageModule)
+  },
 ];
 
 @NgModule({
