298bbacc281077d5763beeaa991a6ce85f63e5dd
Juan Pablo Arr <ju.arriagadar@duocuc.cl>
2024-09-09 12:16:56 -0300

    acciones para el boton

--- src/app/pages/alertas/alertas.page.ts
+++ src/app/pages/alertas/alertas.page.ts
@@ -7,8 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertasPage implements OnInit {
 
+  mensaje:String=''
+
   constructor() { }
-  alertButtons = ['Aceptar'];/* Nombre del boton */
+  alertButtons = [{
+    /* Nombre del boton */
+    text:'Aceptar',
+    handler:()=>{/* Hace cosas despues de apretar el boton */
+      console.log("ACEPTAR!");
+    }
+  },
+  {
+    text:'Cancelar',
+    role:'cancel',
+    handler:()=>{
+      console.log("CERRAR!")
+    }
+  },
+  {
+    text:'Ejecutar Metodo',
+    handler:()=>{
+      this.mensaje='Cambio desde la alerta'
+    }
+  }
+  ];
   ngOnInit() {
   }
 
